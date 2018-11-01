dd862ce7dbe77e49d9ecb1cfefeac961dcd2fb10
Soul Evans <soulevans07@gmail.com>
2018-11-01 23:13:29 +0100

    notification list added to user home

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,29 @@
 class Notification < ApplicationRecord
   belongs_to :user
+
+  def type_string
+    case self.item_type
+    when 0
+      "cocktail"
+    when 1
+      "ingredient"
+    else
+      raise "Cannot get type: wrong item type"
+    end
+  end
+
+  def get_item
+    case self.item_type
+    when 0
+      Cocktail.find_by_id(self.item)
+    when 1
+      Ingredient.find_by_id(self.item)
+    else
+      raise "Cannot get item: wrong item type"
+    end
+  end
+
+  def get_sender
+    User.find_by_id(self.sender)
+  end
 end
