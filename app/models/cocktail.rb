cfba457926a48e69c977c6afe2821b84edac7642
Soul Evans <soulevans07@gmail.com>
2018-11-02 00:07:24 +0100

    missing idicator added

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,4 +2,14 @@ class Cocktail < ApplicationRecord
   belongs_to :owner, class_name: "User", foreign_key: :user_id
   has_and_belongs_to_many :users
   has_and_belongs_to_many :ingredients
+
+  def missing?
+    missing = 0
+    self.ingredients.each do |ingr|
+      unless ingr.have
+        missing += 1
+      end
+    end
+    missing
+  end
 end
